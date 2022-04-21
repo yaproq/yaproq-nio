e9a99d779aaa1347194b43d7cc2217008657e7c4
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-21 22:47:07 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,20 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "yaproq-nio",
-    products: [.library(name: "YaproqNIO", targets: ["YaproqNIO"])],
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v12),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "YaproqNIO", targets: ["YaproqNIO"])
+    ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.29.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(name: "yaproq", url: "https://github.com/yaproq/yaproq.git", .branch("master"))
     ],
     targets: [
@@ -14,10 +22,9 @@ let package = Package(
             .product(name: "NIO", package: "swift-nio"),
             .product(name: "Yaproq", package: "yaproq")
         ]),
-        .testTarget(
-            name: "YaproqNIOTests",
-            dependencies: [.target(name: "YaproqNIO")]
-        )
+        .testTarget(name: "YaproqNIOTests", dependencies: [
+            .target(name: "YaproqNIO")
+        ])
     ],
     swiftLanguageVersions: [.v5]
 )
